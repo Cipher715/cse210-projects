9d15f6632d8cf3436c33cb2ca509b49c1c7a472b
Cipher715 <atsushi.karino@gmail.com>
2024-11-17 01:04:28 +0900

    W2 learning

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        Job job2 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Developer";
+        job1._startYear = 2000;
+        job1._endYear = 2012;
+        job2._company = "Apple";
+        job2._jobTitle = "Infrastructure Engineer";
+        job2._startYear = 2012;
+        job2._endYear = 2022;
+        //job1.Display();
+        //job2.Display();
+
+        Resme myResme = new Resme();
+        myResme._name = "Atsushi Karino";
+        myResme._jobs.Add(job1);
+        myResme._jobs.Add(job2);
+        myResme.Display();
     }
 }
+
